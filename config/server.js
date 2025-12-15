a786e6f41b339accb6888dae7125006d94e5ec6a
Abu Bakar Saddique <138863042+AbuBakar125-co@users.noreply.github.com>
2025-12-15 13:36:57 +0500

    adding blog automatically

--- config/server.js
+++ config/server.js
@@ -2,7 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // Provide an array of app keys. Prefer setting the `APP_KEYS` env var
+    // as a comma-separated list in production. We include a default key
+    // here so Strapi can start in development.
+    keys: env.array('APP_KEYS', [
+      'd71fbde8aa9e43f4e034befd68822bb9c315bbf1a55e19505740c9e473b20ca4',
+    ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
